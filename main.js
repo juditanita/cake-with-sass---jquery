18c210f7eebc955016c7ae2495a21effd9937cf7
juditanita <101408621+juditanita@users.noreply.github.com>
2024-08-05 22:40:15 +0200

    update main

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
 $(document).ready(function () {
-  $("#menuOpen").click(function () {
-    $(".navbar").css("display", "flex");
-    $("#menuOpen").addClass("hidden");
 
-    $("#menuClosed").removeClass("hidden");
-  });
-
-  $("#menuClosed").click(function () {
-    $(".navbar").css("display", "none");
-
-    $("#menuOpen").removeClass("hidden");
-
-    $("#menuClosed").addClass("hidden");
-  });
+ 
 
 
-  $("#menu li a").click(function(){
-   $(".navbar").slideToggle(300);
-   $("#menuClosed").addClass("hidden");
-   $("#menuOpen").removeClass("hidden");
 
-   })
+    if($(window).width() < 700){
+      $("#menu li a").click(function(){
+        $(".navbar").slideToggle(300);
+        $("#menuClosed").addClass("hidden");
+        $("#menuOpen").removeClass("hidden");
+    
+        })
+    }
+   
+   
+  
+      $("#menuOpen").click(function () {
+        $(".navbar").css("display", "flex");
+        $("#menuOpen").addClass("hidden");
+    
+        $("#menuClosed").removeClass("hidden");
+      });
+    
+      $("#menuClosed").click(function () {
+        $(".navbar").css("display", "none");
+    
+        $("#menuOpen").removeClass("hidden");
+    
+        $("#menuClosed").addClass("hidden");
+      });
+      
+     
 
-   $(".slide-in").hide()
+
+      
+
+      
+   
+
+   
+
+
+     
+        
+    
+   
+
+
+  
+
+
+ 
+
+
+  
+
+   $(".slide-in").hide();
    $(".show-img").click(
    function(){
   
